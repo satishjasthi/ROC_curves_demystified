7b4bb3a1885c5a8f5a265a3bee331d2c9ce86eab
satishjasthi <satish27may@gmail.com>
2019-05-08 17:44:39 +0530

    Created Project

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Project to understand what  exactly ROC curve is and who to use it to evaluate classification model',
+    author='Satish Jasthi',
+    license='MIT',
+)
